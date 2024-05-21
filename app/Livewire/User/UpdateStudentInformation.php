7058cef6ade1fd3ef4eb0f9ab61ea5126f5662ae
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2024-05-21 08:11:34 +0330

    add update student information

--- app/Livewire/User/UpdateStudentInformation.php
+++ app/Livewire/User/UpdateStudentInformation.php
@@ -6,6 +6,11 @@ use Livewire\Component;
 
 class UpdateStudentInformation extends Component
 {
+    public function mount()
+    {
+        $this->github = auth()->user()->girhub;
+        $this->student_id = auth()->user()->student_id;
+    }
     public function render()
     {
         return view('livewire.user.update-student-information');
