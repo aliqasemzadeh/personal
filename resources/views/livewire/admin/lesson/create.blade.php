30d9fb85dfdf2acff9d49e924197ecf331607e65
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2024-05-22 14:12:27 +0330

    create lesson page.

--- resources/views/livewire/admin/lesson/create.blade.php
+++ resources/views/livewire/admin/lesson/create.blade.php
@@ -1,3 +1,17 @@
-<div>
-    {{-- Care about people's approval and you will be their prisoner. --}}
+<div class="card">
+    <div class="card-body">
+        <label class="form-control">
+            <div class="label">
+                <span class="label-text">{{ __('Title') }}</span>
+            </div>
+            <input type="text" wire:model="title" placeholder="{{ __('Title') }}" class="input input-bordered" />
+        </label>
+        <label class="form-control">
+            <div class="label">
+                <span class="label-text">{{ __('Description') }}</span>
+            </div>
+            <textarea wire:model="description" class="textarea textarea-bordered h-24" placeholder="{{ __('Description') }}"></textarea>
+        </label>
+        <button class="btn btn-primary">{{ __('Create') }}</button>
+    </div>
 </div>
