ccaac034c7dbb1e452f09950e61000ed31ea794f
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2024-05-31 12:18:54 +0330

    Add new Livewire component for lesson workout
    This update includes the creation of a new workout blade file and a Livewire component named 'Workout'. The Workout component is mounted with a specific lesson id and rends the associated workout view.

---
+++ app/Livewire/Admin/Lesson/Workout.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Livewire\Admin\Lesson;
+
+use App\Models\Lesson;
+use App\Models\StudentWorkout;
+use Livewire\Attributes\On;
+use Livewire\Component;
+
+class Workout extends Component
+{
+    public $lesson_id;
+    public $lesson;
+
+    public function mount($lesson_id)
+    {
+        $this->lesson_id = $lesson_id;
+        $this->lesson = Lesson::findOrFail($lesson_id);
+    }
+
+    #[On('check-workout')]
+    public function render()
+    {
+        $workouts = StudentWorkout::where('lesson_id', $this->lesson_id)->get();
+        return view('livewire.admin.lesson.workout', compact('workouts'));
+    }
+}
