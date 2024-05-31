2f2202ba51ccfab43191a550e1a28456a65a847a
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2024-05-31 08:00:36 +0330

    Add student listing to Livewire Admin component
    The livewire admin component has been updated to retrieve and display a list of all students. This change involves a modification to the render function in the Index.php file and an update to the associated blade template index.blade.php to display the fetched student records.

--- app/Livewire/Admin/Student/Index.php
+++ app/Livewire/Admin/Student/Index.php
@@ -2,12 +2,14 @@
 
 namespace App\Livewire\Admin\Student;
 
+use App\Models\User;
 use Livewire\Component;
 
 class Index extends Component
 {
     public function render()
     {
-        return view('livewire.admin.student.index');
+        $students = User::all();
+        return view('livewire.admin.student.index', compact('students'));
     }
 }
--- resources/views/livewire/admin/student/index.blade.php
+++ resources/views/livewire/admin/student/index.blade.php
@@ -1,3 +1,43 @@
-<div>
-    {{-- Be like water. --}}
+<div class="card">
+    <div class="card-body">
+        <div class="overflow-x-auto">
+            <table class="table">
+                <!-- head -->
+                <thead>
+                <tr>
+                    <th>{{ __('Name') }}</th>
+                    <th>{{ __('Email') }}</th>
+                    <th>{{ __('Student ID') }}</th>
+                    <th>{{ __('GitHub') }}</th>
+                    <th></th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($users as $user)
+                    <!-- row 1 -->
+                    <tr :key="$user->id">
+                        <td>{{ $user->name }}</td>
+                        <td>{{ $user->email }}</td>
+                        <td>{{ $user->student_id }}</td>
+                        <td>{{ $user->github }}</td>
+                        <th>
+
+                        </th>
+                    </tr>
+                @endforeach
+                </tbody>
+                <!-- foot -->
+                <tfoot>
+                <tr>
+                    <th>{{ __('Name') }}</th>
+                    <th>{{ __('Email') }}</th>
+                    <th>{{ __('Student ID') }}</th>
+                    <th>{{ __('GitHub') }}</th>
+                    <th></th>
+                </tr>
+                </tfoot>
+
+            </table>
+        </div>
+    </div>
 </div>
