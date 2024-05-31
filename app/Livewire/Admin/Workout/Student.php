05133c584006969b7527053bce526eb3e652991b
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2024-05-31 12:06:27 +0330

    Update workout view and filter null URLs
    The view of the workout for students has been updated, now presenting the information in a more structured way, through a table within a card component. Also, the query retrieving student's workouts has been adjusted to filter out those entries with null URLs. This improves data organization and presentation for better user experience.

--- app/Livewire/Admin/Workout/Student.php
+++ app/Livewire/Admin/Workout/Student.php
@@ -17,7 +17,7 @@ class Student extends Component
 
     public function render()
     {
-        $studentWorkouts = StudentWorkout::where('workout_id', $this->workout->id)->get();
+        $studentWorkouts = StudentWorkout::where('workout_id', $this->workout->id)->where('url', '!=', 'NULL')->get();
         return view('livewire.admin.workout.student', compact('studentWorkouts'));
     }
 }
--- resources/views/livewire/admin/workout/student.blade.php
+++ resources/views/livewire/admin/workout/student.blade.php
@@ -1,7 +1,39 @@
-<div>
-    <h3>{!! $workout->description !!}</h3>
-    @foreach($studentWorkouts as $studentWorkout)
-        {{ $studentWorkout->url }}
-        <br />
-    @endforeach
+<div class="card">
+    <div class="card-body">
+        <h2 class="card-title">{!! $workout->description !!}</h2>
+        <div class="overflow-x-auto">
+            <table class="table">
+                <!-- head -->
+                <thead>
+                <tr>
+                    <th>{{ __('URL') }}</th>
+                    <th></th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($studentWorkouts as $studentWorkout)
+                    <!-- row 1 -->
+                    <tr :key="$workout->id">
+                        <td>
+                            <a target="_blank" class="btn" href="{!! nl2br($studentWorkout->url) !!}">
+                            {{ __('View') }}
+                            </a>
+                        </td>
+                        <th>
+
+                        </th>
+                    </tr>
+                @endforeach
+                </tbody>
+                <!-- foot -->
+                <tfoot>
+                <tr>
+                    <th>{{ __('URL') }}</th>
+                    <th></th>
+                </tr>
+                </tfoot>
+
+            </table>
+        </div>
+    </div>
 </div>
