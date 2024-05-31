80fa7c2b1a50fac6b51f1cef92d47d995c241e06
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2024-05-31 11:55:19 +0330

    Fix formatting in student workout view
    This commit removes an extra character from the 'foreach' statement in the student workout view file. It also adds a line break after each URL for better readability.

--- resources/views/livewire/admin/workout/student.blade.php
+++ resources/views/livewire/admin/workout/student.blade.php
@@ -1,6 +1,7 @@
 <div>
     <h3>{!! $workout->description !!}</h3>
-    @foreach($studentWorkouts as $studentWorkout)«
+    @foreach($studentWorkouts as $studentWorkout)
         {{ $studentWorkout->url }}
+        <br />
     @endforeach
 </div>
