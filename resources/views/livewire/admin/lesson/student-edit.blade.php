de6786b6a331be62a40e2eb712530ab27c7a11cb
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2024-06-13 01:19:51 +0330

    Add error messages and refactor form structure
    The changes include introducing error messages in the student-edit form. Specifically, after each input field, an error message displays if input validation fails. Additionally, the forms' structures in the student-edit and student views have been refactored by encapsulating the whole form elements inside a div block for proper alignment.

--- resources/views/livewire/admin/lesson/student-edit.blade.php
+++ resources/views/livewire/admin/lesson/student-edit.blade.php
@@ -1,40 +1,47 @@
-<form wire:submit="save">
-    <label class="form-control w-full max-w-xs">
-        <div class="label">
-            <span class="label-text">{{ __('Midterm') }}</span>
-        </div>
-        <input type="text" wire:model="midterm" placeholder="{{ __('Midterm') }}" class="input input-bordered w-full max-w-xs" />
-    </label>
+<div>
+    <form wire:submit="save">
+        <label class="form-control w-full max-w-xs">
+            <div class="label">
+                <span class="label-text">{{ __('Midterm') }}</span>
+            </div>
+            <input type="text" wire:model="midterm" placeholder="{{ __('Midterm') }}" class="input input-bordered w-full max-w-xs" />
+            <div>@error('midterm') <span class="text-red-700">{{ $message }}</span> @enderror</div>
+        </label>
 
-    <label class="form-control w-full max-w-xs">
-        <div class="label">
-            <span class="label-text">{{ __('Final') }}</span>
-        </div>
-        <input type="text" wire:model="final" placeholder="{{ __('Final') }}" class="input input-bordered w-full max-w-xs" />
-    </label>
+        <label class="form-control w-full max-w-xs">
+            <div class="label">
+                <span class="label-text">{{ __('Final') }}</span>
+            </div>
+            <input type="text" wire:model="final" placeholder="{{ __('Final') }}" class="input input-bordered w-full max-w-xs" />
+            <div>@error('final') <span class="text-red-700">{{ $message }}</span> @enderror</div>
+        </label>
 
-    <label class="form-control w-full max-w-xs">
-        <div class="label">
-            <span class="label-text">{{ __('Absence') }}</span>
-        </div>
-        <input type="text" wire:model="absence" placeholder="{{ __('Absence') }}" class="input input-bordered w-full max-w-xs" />
-    </label>
+        <label class="form-control w-full max-w-xs">
+            <div class="label">
+                <span class="label-text">{{ __('Absence') }}</span>
+            </div>
+            <input type="text" wire:model="absence" placeholder="{{ __('Absence') }}" class="input input-bordered w-full max-w-xs" />
+            <div>@error('absence') <span class="text-red-700">{{ $message }}</span> @enderror</div>
+        </label>
 
-    <label class="form-control w-full max-w-xs">
-        <div class="label">
-            <span class="label-text">{{ __('Plus') }}</span>
-        </div>
-        <input type="text" wire:model="plus" placeholder="{{ __('Plus') }}" class="input input-bordered w-full max-w-xs" />
-    </label>
+        <label class="form-control w-full max-w-xs">
+            <div class="label">
+                <span class="label-text">{{ __('Plus') }}</span>
+            </div>
+            <input type="text" wire:model="plus" placeholder="{{ __('Plus') }}" class="input input-bordered w-full max-w-xs" />
+            <div>@error('plus') <span class="text-red-700">{{ $message }}</span> @enderror</div>
+        </label>
 
-    <label class="form-control w-full max-w-xs">
-        <div class="label">
-            <span class="label-text">{{ __('Conferences') }}</span>
-        </div>
-        <input type="text" wire:model="conferences" placeholder="{{ __('Conferences') }}" class="input input-bordered w-full max-w-xs" />
-    </label>
-    <button type="submit" class="btn btn-primary">{{ __('Submit') }}</button>
-    <x-action-message class="me-3" on="saved">
-        {{ __('Saved.') }}
-    </x-action-message>
-</form>
+        <label class="form-control w-full max-w-xs">
+            <div class="label">
+                <span class="label-text">{{ __('Conferences') }}</span>
+            </div>
+            <input type="text" wire:model="conferences" placeholder="{{ __('Conferences') }}" class="input input-bordered w-full max-w-xs" />
+            <div>@error('conferences') <span class="text-red-700">{{ $message }}</span> @enderror</div>
+        </label>
+        <button type="submit" class="btn btn-primary">{{ __('Submit') }}</button>
+        <x-action-message class="me-3" on="saved">
+            {{ __('Saved.') }}
+        </x-action-message>
+    </form>
+</div>
--- resources/views/livewire/admin/lesson/student.blade.php
+++ resources/views/livewire/admin/lesson/student.blade.php
@@ -1,24 +1,26 @@
-<form wire:submit="import">
-    <div class="card">
-        <div class="card-body">
+<div class="card">
+    <div class="card-body">
 
-            <ul class="list-disc list-inside space-y-2">
-                @foreach($students as $student)
-                <li class="text-blue-500" wire:key="{{ $student->student_id }}">
+        <ul class="list-disc list-inside space-y-2">
+            @foreach($students as $student)
+                <li class="text-blue-500">
                     {{ $student->student_id }}
                     <br />
-                        <livewire:admin.lesson.student-edit :student="$student" :key="$student->student_id" />
+                    <livewire:admin.lesson.student-edit :$student :key="$student->student_id" />
+                    <x-section-border />
                 </li>
-                @endforeach
-            </ul>
-            <label class="form-control">
-                <div class="label">
-                    <span class="label-text">{{ __('File') }}</span>
-                </div>
+            @endforeach
+        </ul>
+        <label class="form-control">
+            <div class="label">
+                <span class="label-text">{{ __('File') }}</span>
+            </div>
+            <form wire:submit="import">
                 <input type="file" wire:model="file" class="file-input file-input-bordered" />
                 @error('file') <span class="text-red-700">{{ $message }}</span> @enderror
-            </label>
-            <button class="btn btn-primary">{{ __('Upload') }}</button>
-        </div>
+            </form>
+        </label>
+        <button class="btn btn-primary">{{ __('Upload') }}</button>
     </div>
-</form>
+</div>
+
