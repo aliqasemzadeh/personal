3fecd2dd8538a7d75f827593adc68f30f8ccf0d2
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2024-06-13 00:06:59 +0330

    Add student edit functionality in Livewire component
    A new Livewire component has been created for editing student data pertaining to lessons. This component includes a 'mount' function to initialize a given student object, and a 'save' function has been added for future implementation.

--- app/Livewire/Admin/Lesson/StudentEdit.php
+++ app/Livewire/Admin/Lesson/StudentEdit.php
@@ -2,10 +2,23 @@
 
 namespace App\Livewire\Admin\Lesson;
 
+use App\Models\LessonStudent;
 use Livewire\Component;
 
 class StudentEdit extends Component
 {
+    public LessonStudent $student;
+
+    public function mount(LessonStudent $student)
+    {
+        $this->student = $student;
+    }
+
+    public function save()
+    {
+
+    }
+
     public function render()
     {
         return view('livewire.admin.lesson.student-edit');
