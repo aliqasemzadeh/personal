9b4882de16664b057f07563aab35b19f5f835975
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2024-06-13 00:21:44 +0330

    Update form labels and validation in student edit component
    In the student edit component, form label names have been updated by capitalizing the first letter and making them translatable. Also, corresponding public properties and validation rules were added to the Livewire's StudentEdit component in support for the updated form fields.

--- app/Livewire/Admin/Lesson/StudentEdit.php
+++ app/Livewire/Admin/Lesson/StudentEdit.php
@@ -8,14 +8,39 @@ use Livewire\Component;
 class StudentEdit extends Component
 {
     public LessonStudent $student;
+    public $midterm;
+    public $final;
+    public $absence;
+    public $plus;
+    public $conferences;
 
     public function mount(LessonStudent $student)
     {
         $this->student = $student;
+        $this->midterm = $student->midterm;
+        $this->final = $student->final;
+        $this->absence = $this->absence;
+        $this->plus = $this->plus;
+        $this->conferences = $this->conferences;
+
     }
 
     public function save()
     {
+        $this->validate([
+            'midterm' => 'required',
+            'final' => 'required',
+            'absence' => 'required',
+            'plus' => 'required',
+            'conferences' => 'required',
+        ]);
+
+        $this->student->midterm = $this->midterm;
+        $this->student->final = $this->final;
+        $this->student->absence = $this->absence;
+        $this->student->plus = $this->plus;
+        $this->student->conferences = $this->conferences;
+        $this->student->save();
 
     }
 
--- resources/views/livewire/admin/lesson/student-edit.blade.php
+++ resources/views/livewire/admin/lesson/student-edit.blade.php
@@ -1,37 +1,37 @@
 <form wire:submit="save">
     <label class="form-control w-full max-w-xs">
         <div class="label">
-            <span class="label-text">{{ __('midterm') }}</span>
+            <span class="label-text">{{ __('Midterm') }}</span>
         </div>
-        <input type="text" wire:model="midterm" placeholder="midterm" class="input input-bordered w-full max-w-xs" />
+        <input type="text" wire:model="midterm" placeholder="{{ __('Midterm') }}" class="input input-bordered w-full max-w-xs" />
     </label>
 
     <label class="form-control w-full max-w-xs">
         <div class="label">
-            <span class="label-text">{{ __('final') }}</span>
+            <span class="label-text">{{ __('Final') }}</span>
         </div>
-        <input type="text" wire:model="final" placeholder="final" class="input input-bordered w-full max-w-xs" />
+        <input type="text" wire:model="final" placeholder="{{ __('Final') }}" class="input input-bordered w-full max-w-xs" />
     </label>
 
     <label class="form-control w-full max-w-xs">
         <div class="label">
-            <span class="label-text">{{ __('absence') }}</span>
+            <span class="label-text">{{ __('Absence') }}</span>
         </div>
-        <input type="text" wire:model="absence" placeholder="absence" class="input input-bordered w-full max-w-xs" />
+        <input type="text" wire:model="absence" placeholder="{{ __('Absence') }}" class="input input-bordered w-full max-w-xs" />
     </label>
 
     <label class="form-control w-full max-w-xs">
         <div class="label">
-            <span class="label-text">{{ __('plus') }}</span>
+            <span class="label-text">{{ __('Plus') }}</span>
         </div>
-        <input type="text" wire:model="plus" placeholder="plus" class="input input-bordered w-full max-w-xs" />
+        <input type="text" wire:model="plus" placeholder="{{ __('Plus') }}" class="input input-bordered w-full max-w-xs" />
     </label>
 
     <label class="form-control w-full max-w-xs">
         <div class="label">
-            <span class="label-text">{{ __('conferences') }}</span>
+            <span class="label-text">{{ __('Conferences') }}</span>
         </div>
-        <input type="text" wire:model="conferences" placeholder="conferences" class="input input-bordered w-full max-w-xs" />
+        <input type="text" wire:model="conferences" placeholder="{{ __('Conferences') }}" class="input input-bordered w-full max-w-xs" />
     </label>
     <button class="btn btn-primary">{{ __('Submit') }}</button>
 </form>
