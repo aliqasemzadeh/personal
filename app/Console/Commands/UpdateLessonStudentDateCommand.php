c3f5d80c716b803f53ef7ad39e889359a4dd260c
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2024-06-12 23:44:36 +0330

    Update User lookup in UpdateLessonStudentDateCommand
    The User lookup has been adjusted to fetch users by 'student_id' before counting the correct and incorrect workouts. This change allows actions to be performed only when the related user object exists, resulting in more accurate and reliable code execution.

--- app/Console/Commands/UpdateLessonStudentDateCommand.php
+++ app/Console/Commands/UpdateLessonStudentDateCommand.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use App\Models\Lesson;
 use App\Models\LessonWorkout;
 use App\Models\StudentWorkout;
+use App\Models\User;
 use Illuminate\Console\Command;
 
 class UpdateLessonStudentDateCommand extends Command
@@ -34,13 +35,17 @@ class UpdateLessonStudentDateCommand extends Command
             foreach ($lesson->students as $student) {
                 echo "Student " . $student->student_id."\n";
                 $workout_total = LessonWorkout::where('lesson_id', $lesson->id)->count();
-                $workout_right = StudentWorkout::where('lesson_id', $lesson->id)->where('student_id', $student->student_id)->where('check', 1)->count();
-                $workout_wrong = StudentWorkout::where('lesson_id', $lesson->id)->where('student_id', $student->student_id)->where('check', -1)->count();
+                if($user = User::where('student_id', $student->student_id)->first()) {
+                    $workout_right = StudentWorkout::where('lesson_id', $lesson->id)->where('student_id', $user->id)->where('check', 1)->count();
+                    $workout_wrong = StudentWorkout::where('lesson_id', $lesson->id)->where('student_id', $user->id)->where('check', -1)->count();
 
-                $student->workout_total = $workout_total;
-                $student->workout_right = $workout_right;
-                $student->workout_wrong = $workout_wrong;
-                $student->save();
+
+                    $student->workout_total = $workout_total;
+                    $student->workout_right = $workout_right;
+                    $student->workout_wrong = $workout_wrong;
+                    $student->save();
+                }
+
 
             }
         }
