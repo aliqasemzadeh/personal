dc83cbd5934bbfb9b579261af0fce313f1fea65b
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2024-06-12 22:30:32 +0330

    Add count workout migration to lesson_students
    A new migration is added for the 'lesson_students' table. This migration prepares the framework to monitor and keep track of workout totals, rights, wrongs, points, and conferences, which are to be rolled back if necessary.

---
+++ database/migrations/2024_06_12_185315_add_count_workout_to_lesson_students.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('lesson_students', function (Blueprint $table) {
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('lesson_students', function (Blueprint $table) {
+            $table->double('workout_total')->default(0);
+            $table->double('workout_right')->default(0);
+            $table->double('workout_wrong')->default(0);
+            $table->double('workout_point')->default(0);
+            $table->double('conferences')->default(0);
+        });
+    }
+};
